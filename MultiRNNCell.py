dd692f739b58b2a45180260dfa34c95caaa838ee
HUANG Kung-hsiang <khuangaf@dycpu1.cse.ust.hk>
2017-07-03 19:34:15 +0800

    visualization

---
+++ MultiRNNCell.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import hashlib
+import numbers
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import tensor_util
+from tensorflow.python.layers import base as base_layer
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import clip_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import partitioned_variables
+from tensorflow.python.ops import random_ops
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import variables as tf_variables
+from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.util import nest
+
+class MultiRNNCell(RNNCell):
+  """RNN cell composed sequentially of multiple simple cells."""
+
+  def __init__(self, cells, state_is_tuple=True):
+    """Create a RNN cell composed sequentially of a number of RNNCells.
+    Args:
+      cells: list of RNNCells that will be composed in this order.
+      state_is_tuple: If True, accepted and returned states are n-tuples, where
+        `n = len(cells)`.  If False, the states are all
+        concatenated along the column axis.  This latter behavior will soon be
+        deprecated.
+    Raises:
+      ValueError: if cells is empty (not allowed), or at least one of the cells
+        returns a state tuple but the flag `state_is_tuple` is `False`.
+    """
+    super(MultiRNNCell, self).__init__()
+    if not cells:
+      raise ValueError("Must specify at least one cell for MultiRNNCell.")
+    if not nest.is_sequence(cells):
+      raise TypeError(
+          "cells must be a list or tuple, but saw: %s." % cells)
+
+    self._cells = cells
+    self._state_is_tuple = state_is_tuple
+    if not state_is_tuple:
+      if any(nest.is_sequence(c.state_size) for c in self._cells):
+        raise ValueError("Some cells return tuples of states, but the flag "
+                         "state_is_tuple is not set.  State sizes are: %s"
+                         % str([c.state_size for c in self._cells]))
+
+  @property
+  def state_size(self):
+    if self._state_is_tuple:
+      return tuple(cell.state_size for cell in self._cells)
+    else:
+      return sum([cell.state_size for cell in self._cells])
+
+  @property
+  def output_size(self):
+    return self._cells[-1].output_size
+
+  def zero_state(self, batch_size, dtype):
+    with ops.name_scope(type(self).__name__ + "ZeroState", values=[batch_size]):
+      if self._state_is_tuple:
+        return tuple(cell.zero_state(batch_size, dtype) for cell in self._cells)
+      else:
+        # We know here that state_size of each cell is not a tuple and
+        # presumably does not contain TensorArrays or anything else fancy
+        return super(MultiRNNCell, self).zero_state(batch_size, dtype)
+
+  def call(self, inputs, state):
+    """Run this multi-layer cell on inputs, starting from state."""
+    cur_state_pos = 0
+    cur_inp = inputs
+    new_states = []
+    for i, cell in enumerate(self._cells):
+      with vs.variable_scope("cell_%d" % i):
+        if self._state_is_tuple:
+          if not nest.is_sequence(state):
+            raise ValueError(
+                "Expected state to be a tuple of length %d, but received: %s" %
+                (len(self.state_size), state))
+          cur_state = state[i]
+        else:
+          cur_state = array_ops.slice(state, [0, cur_state_pos],
+                                      [-1, cell.state_size])
+          cur_state_pos += cell.state_size
+        cur_inp, new_state = cell(cur_inp, cur_state)
+        new_states.append(new_state)
+
+    new_states = (tuple(new_states) if self._state_is_tuple else
+                  array_ops.concat(new_states, 1))
+
+    return cur_inp, new_states
+
+
--- student_model.py
+++ student_model.py
@@ -1,4 +1,4 @@
-# The code is rewritten based on source code from tensorflow tutorial for Recurrent Neural Network.
+    # The code is rewritten based on source code from tensorflow tutorial for Recurrent Neural Network.
 # https://www.tensorflow.org/versions/0.6.0/tutorials/recurrent/index.html
 # You can get source code for the tutorial from
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/rnn/ptb/ptb_word_lm.py
@@ -29,12 +29,16 @@ tf.flags.DEFINE_integer("hidden_layer_num", 1, "The number of hidden layers (Int
 tf.flags.DEFINE_integer("hidden_size", 200, "The number of hidden nodes (Integer)")
 tf.flags.DEFINE_integer("evaluation_interval", 5, "Evaluate and print results every x epochs")
 tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
-tf.flags.DEFINE_integer("epochs", 150, "Number of epochs to train for.")
+tf.flags.DEFINE_integer("epochs", 30, "Number of epochs to train for.")
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-tf.flags.DEFINE_string("train_data_path", 'data/trainTemp.csv', "Path to the training dataset")
-tf.flags.DEFINE_string("test_data_path", 'data/testTemp.csv', "Path to the testing dataset")
+tf.flags.DEFINE_string("train_data_path", 'data/0910_b_train.csv', "Path to the training dataset")
+tf.flags.DEFINE_string("test_data_path", 'data/0910_b_test.csv', "Path to the testing dataset")
 
+currentOutputMatrix = []
+cumulativeOutputMatrix = []
+log_file_path = '1layeredb.txt'
+hidden_state_path = 'hidden_state.csv'
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
 print("\nParameters:")
@@ -68,7 +72,7 @@ class StudentModel(object):
         inputs = self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
         self._target_id = target_id = tf.placeholder(tf.int32, [None])
         self._target_correctness = target_correctness = tf.placeholder(tf.float32, [None])
-        final_hidden_size = size
+        final_hidden_size = self.state_size[-1]
 
         hidden_layers = []
         
@@ -81,12 +85,21 @@ class StudentModel(object):
         
         cell = tf.contrib.rnn.MultiRNNCell(hidden_layers, state_is_tuple=True)
 
+        #input_data: [batch_size*num_steps]
         input_data = tf.reshape(self._input_data, [-1])
         #one-hot encoding
-        with tf.device("/cpu:0"):
+        with tf.device("/gpu:0"):
+            #labels: [batch_size* num_steps, 1]
             labels = tf.expand_dims(input_data, 1)
+            #indices: [batch_size*num_steps, 1]
             indices = tf.expand_dims(tf.range(0, batch_size*num_steps, 1), 1)
+            #concated: [batch_size * num_steps, 2]
             concated = tf.concat( [indices, labels],1)
+
+            # If sparse_indices is an n by d matrix, then for each i in [0, n)
+            # dense[sparse_indices[i][0], ..., sparse_indices[i][d-1]] = sparse_values[i]
+            # input_size: 2* num_skills
+            # inputs: [batch_size* num_steps * input_size]
             inputs = tf.sparse_to_dense(concated, tf.stack([batch_size*num_steps, input_size]), 1.0, 0.0)
             inputs.set_shape([batch_size*num_steps, input_size])
 
@@ -101,15 +114,28 @@ class StudentModel(object):
         # x = tf.split(0, num_steps, x)
         #inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, num_steps, inputs)]
         #outputs, state = tf.nn.rnn(hidden1, x, dtype=tf.float32)
+        #outputs: [batch_size, num_steps, final_hidden_size]
         outputs, state = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
+        # print state
+        #output = [batch_size * num_steps, final_hidden_size]
         output = tf.reshape(tf.concat(outputs,1), [-1, final_hidden_size])
+        # print "shape"
+        # print outputs.shape
+        # print output.shape
         # calculate the logits from last hidden layer to output layer
+
         sigmoid_w = tf.get_variable("sigmoid_w", [final_hidden_size, num_skills])
         sigmoid_b = tf.get_variable("sigmoid_b", [num_skills])
-        logits = tf.matmul(output, sigmoid_w) + sigmoid_b
 
+        #logits [batch_size * num_steps, num_skills]
+        logits = tf.matmul(output, sigmoid_w) + sigmoid_b
+        
         # from output nodes to pick up the right one we want
+        #logits: [batch_size * num_steps * num_skills]
+        # logits are the output of the rnn after sigmoid
         logits = tf.reshape(logits, [-1])
+        #target_id = batch_num*m.num_steps*m.num_skills + skill_num*m.num_skills + int(problem_ids[j+1]))
+        #selected_logits: shape of target_id
         selected_logits = tf.gather(logits, self.target_id)
 
         #make prediction
@@ -119,6 +145,7 @@ class StudentModel(object):
         loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits = selected_logits,labels= target_correctness))
 
         #self._cost = cost = tf.reduce_mean(loss)
+        self._final_state = state
         self._cost = cost = loss
 
     @property
@@ -169,12 +196,13 @@ class HyperParamsConfig(object):
     max_max_epoch = FLAGS.epochs
     keep_prob = FLAGS.keep_prob
     num_skills = 0
-    state_size = [200,200,200]
+    state_size = [200]
 
 def run_epoch(session, m, students, eval_op, verbose=False):
     """Runs the model on the given data."""
     start_time = time.time()
-
+    global cumulativeOutputMatrix
+    global currentOutputMatrix
     index = 0
     pred_labels = []
     actual_labels = []
@@ -199,25 +227,63 @@ def run_epoch(session, m, students, eval_op, verbose=False):
                 target_correctness.append(int(correctness[j+1]))
                 actual_labels.append(int(correctness[j+1]))
 
-        index += m.batch_size
+        
 
-        pred, _ = session.run([m.pred, eval_op], feed_dict={
+        pred, _, final_state = session.run([m.pred, eval_op, m.final_state], feed_dict={
             m.input_data: x, m.target_id: target_id,
             m.target_correctness: target_correctness})
+        #h: [batch_size, num_unit]
+        h = final_state[0][1]
+        index += m.batch_size
+        # if first batch of data
+        if len(currentOutputMatrix) < 1:
+            currentOutputMatrix = h
+        else:
+            currentOutputMatrix = np.concatenate((currentOutputMatrix, h), axis = 0)
+        # if last iteration in a epoch
+        if index+m.batch_size >= len(students):
+            # if first epoch
+            if len(cumulativeOutputMatrix) < 1:
+                cumulativeOutputMatrix = currentOutputMatrix
+            else:
+                # print cumulativeOutputMatrix.shape
+                # print currentOutputMatrix.shape
+                #ignore test case
+                if np.array(cumulativeOutputMatrix).shape[0] == np.array(currentOutputMatrix).shape[0]:
+
+                    cumulativeOutputMatrix = np.concatenate((cumulativeOutputMatrix, currentOutputMatrix), axis= 1)
+        # a new epoch
+        if index < 1:            
+            currentOutputMatrix = []
+        
+
 
         for p in pred:
             pred_labels.append(p)
-    #print pred_labels
+        
+    # print "final_state"
+    #final_state: (tuple(LSTMStateTuple([batch_size, hidden_size])))
+    # print len(final_state)
+    # print len(final_state[0])
+
+    # print len(currentOutputMatrix)
+    # print len(cumulativeOutputMatrix)
+    # print np.array(cumulativeOutputMatrix).shape
+    # print np.array(currentOutputMatrix).shape
+    # reset current when finish a epoch
+    currentOutputMatrix = []
+    # print final_state[0][0].shape
     rmse = sqrt(mean_squared_error(actual_labels, pred_labels))
     fpr, tpr, thresholds = metrics.roc_curve(actual_labels, pred_labels, pos_label=1)
     auc = metrics.auc(fpr, tpr)
 
     #calculate r^2
     r2 = r2_score(actual_labels, pred_labels)
-    return rmse, auc, r2
+    return rmse, auc, r2, final_state
 
 
 def read_data_from_csv_file(fileName):
+
     config = HyperParamsConfig()
     inputs = []
     targets = []
@@ -267,6 +333,7 @@ def read_data_from_csv_file(fileName):
     return tuple_rows, max_num_problems, max_skill_num+1
 
 def main(unused_args):
+    global cumulativeOutputMatrix
     config = HyperParamsConfig()
     eval_config = HyperParamsConfig()
     timestamp = str(time.time())
@@ -322,23 +389,33 @@ def main(unused_args):
                 f.write("Batch size: %d \n" % (FLAGS.batch_size))
                 f.write("Max grad norm: %d \n" % (FLAGS.max_grad_norm))
             # saver = tf.train.Saver(tf.all_variables())
-
+            cs = []
+            hs = []
             for i in range(config.max_max_epoch):
-                rmse, auc, r2 = run_epoch(session, m, train_students, train_op, verbose=True)
+                rmse, auc, r2, final_state = run_epoch(session, m, train_students, train_op, verbose=True)
                 print("Epoch: %d Train Metrics:\n rmse: %.3f \t auc: %.3f \t r2: %.3f \n" % (i + 1, rmse, auc, r2))
-
+                with open(log_file_path, "a+") as f:
+                    f.write("Epoch: %d Train Metrics:\n rmse: %.3f \t auc: %.3f \t r2: %.3f \n" % (i + 1, rmse, auc, r2))
                 if((i+1) % FLAGS.evaluation_interval == 0):
                     print "Save variables to disk"
-                    # save_path = saver.save(session, model_name)
+                    # save_path = saver.save(session, model_name)#
                     print("*"*10)
                     print("Start to test model....")
-                    rmse, auc, r2 = run_epoch(session, mtest, test_students, tf.no_op())
+                    rmse, auc, r2, _ = run_epoch(session, mtest, test_students, tf.no_op())
                     print("Epoch: %d Test Metrics:\n rmse: %.3f \t auc: %.3f \t r2: %.3f" % (i+1, rmse, auc, r2))
-                    with open(result_file_path, "a+") as f:
-                        f.write("Epoch: %d Test Metrics:\n rmse: %.3f \t auc: %.3f \t r2: %.3f" % ((i+1)/2, rmse, auc, r2))
+                    with open(log_file_path, "a+") as f:
+                        f.write("Epoch: %d Test Metrics:\n rmse: %.3f \t auc: %.3f \t r2: %.3f" % ((i+1) , rmse, auc, r2))
                         f.write("\n")
 
                         print("*"*10)
 
+                # c, h = final_state
+                # if len(cs) < 1:
+                #     cs = c
+                # else 
+    cumulativeOutputMatrix = np.array(cumulativeOutputMatrix)
+    print cumulativeOutputMatrix.shape
+    np.save('hidden_state.npy', cumulativeOutputMatrix)
+    # np.savetxt(hidden_state_path, cumulativeOutputMatrix, delimiter=',',)
 if __name__ == "__main__":
     tf.app.run()
